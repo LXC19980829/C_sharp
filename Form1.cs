317d55db6e2807b2294fc09da036d2a0c154e519
LXC19980829 <1807494057@qq.com>
2021-09-30 11:40:10 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+   public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello world!");
+            MessageBox.Show("显示信息内容", "这是标题");
+            string S = "12345678";
+            textBox1.Text = S[2].ToString();
+        }
+
+      
+    }
+}
